c6c093c3c2b5cb0d7b448a839a1226c3f0cdbe59
diggersheep <alexandre.combeau@outlook.fr>
2017-06-10 22:05:28 +0200

    change 'loop' by 'while true'

--- src/warden.cr
+++ src/warden.cr
@@ -8,9 +8,9 @@ class OptionParser
     # exactly like original but colorized
     private def append_flag(flag, description)
         if flag.size >= 33
-            @flags << "    #{flag.colorize(:cyan)}\n#{" " * 37}#{description}"
+            @flags << "    #{flag.colorize(:light_gray)}\n#{" " * 37}#{description}"
         else
-            @flags << "    #{flag.colorize(:cyan)}#{" " * (33 - flag.size)}#{description}"
+            @flags << "    #{flag.colorize(:light_gray)}#{" " * (33 - flag.size)}#{description}"
         end
     end
 end
@@ -25,7 +25,8 @@ banner += "#{"[-h|--help]".colorize(:light_green)}"
 
 if ARGV[0]?
     OptionParser.parse! do |opt|
-        opt.banner = banner
+        #opt.banner = banner
+        opt.banner = "help : "
         opt.on "-v", "--version", "Show the version" do
             Warden.print_version
             exit
@@ -43,6 +44,19 @@ if ARGV[0]?
         opt.on "-d DELAY", "--delay=DELAY", "Show this help" do |delay|
             begin
                 config.delay = delay.to_u32
+            rescue
+                puts opt
+                exit 1
+            end
+        end
+
+        opt.on "-t TIMEOUT", "--timeout=TIMEOUT", "Timeout in ms for command " do |timeout|
+            begin
+                t = timeout.to_u32
+                if t < 250_u32
+                    t = 250_u32
+                end
+                exit 0
             rescue
                 puts opt
                 exit 1
@@ -67,10 +81,6 @@ end
 
 # PROJECT CONFIG FILE
 project = Config.load_project? config.target
-
-{% if :windows %}
-    puts "win"
-{% end %}
 
 # WATCHER
 watcher = Warden::Watcher.new config, project
