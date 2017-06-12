33ab0ca985a46fc6f24d5b9354f34baa99be9b85
diggersheep <alexandre.combeau@outlook.fr>
2017-06-12 20:43:48 +0200

    a

--- install/linux.sh
+++ install/linux.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+
+
+home=$(echo $HOME)
+name="/root"
+
+if ( $home !== $name ) ; then
+    echo "this script must be"
+    exit 1
+fi
+
 [ -d /usr/share/warden/ ] || mkdir /usr/share/warden/  --verbose
 
-
 echo "installation:"
+
+echo "RELEASE" > ./RELEASE
 
 cp ./config.yml /usr/share/warden/config.yml
 if [[ -f /usr/share/warden/config.yml ]] ; then
@@ -28,10 +39,13 @@ fi
 cp ./bin/warden /usr/bin/warden
 if [[ -f /usr/bin/warden ]] ; then
     echo "  - bin/warden ----------> /usr/bin/warden"
+else
     echo
     echo "Installation failed!"
     . uninstall/linux.sh
     exit 1
 fi
+
+rm ./RELEASE
 
 echo "Installation finished"
