26165d2e6d1c55d8bf588089e226bd9e2f582956
diggersheep <alexandre.combeau@outlook.fr>
2017-07-05 19:17:59 +0200

    comment this little script

--- uninstall/linux.sh
+++ uninstall/linux.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "uninstallation:"
+
+# remove warden directory
 if [[ -d /usr/share/warden ]] ; then
     rm -r /usr/share/warden/ 2> /dev/null
     echo "  - deletion of the directory /usr/share/warden/"
 fi
 
+# remove binary
 if [[ -f /usr/bin/warden ]] ; then
     rm /usr/bin/warden 2> /dev/null
     echo "  - deletion of the binary /usr/bin/warden"
