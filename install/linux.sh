2c2d9d4e0cac8866d41438d995884b55212d2fcf
diggersheep <alexandre.combeau@outlook.fr>
2017-07-05 19:17:03 +0200

    comment this little script

--- install/linux.sh
+++ install/linux.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# create new directory
 [ -d /usr/share/warden/ ] || mkdir /usr/share/warden/  --verbose
 
 echo "installation:"
 
+# fill RELEASE for ... release :P
 echo "RELEASE" > ./RELEASE
 
+# build the release
 echo "  - building a release of Warden"
 crystal build ./src/warden.cr -o ./bin/warden --release
 
-
+# move config file
 cp ./config.yml /usr/share/warden/config.yml
 if [[ -f /usr/share/warden/config.yml ]] ; then
     echo "  - config.yml ----------> /usr/share/warden/config.yml"
@@ -20,6 +23,7 @@ else
     exit 1
 fi
 
+# move uninstallation file
 cp ./uninstall/linux.sh /usr/share/warden/uninstall.sh
 if [[ -f /usr/share/warden/uninstall.sh ]] ; then
     echo "  - uninstall/linux.sh --> /usr/share/warden/uninstall.sh"
@@ -30,6 +34,7 @@ else
     exit 1
 fi
 
+# move binary
 cp ./bin/warden /usr/bin/warden
 if [[ -f /usr/bin/warden ]] ; then
     echo "  - bin/warden ----------> /usr/bin/warden"
@@ -40,6 +45,9 @@ else
     exit 1
 fi
 
+# empty RELEASE
 echo > ./RELEASE
 
+
+# show a super message!
 echo "Installation finished"
